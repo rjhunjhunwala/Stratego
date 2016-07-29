e3ea7d0486c1ba55cbfdcc6e886c919cc5f292dc
Rohan Jhunjhunwala <rjhunjhunwala80@malvernprep.org>
2016-07-28 20:48:08 -0400

    Add files via upload

---
+++ Stratego.java
@@ -0,0 +1,23 @@
+     /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stratego;
+
+
+
+/**
+ *
+ * @author rohan
+ */
+public class Stratego {
+/**
+	* Rules of Stratego have been modified to better suit Play by artificial Intelligence
+	* 1. NO LAKES (CHOKEPOINTS)
+	* 2. Twenty piece armies (2 bombs one flag, one spy, one nine and one ten, two of the rest 2-8)
+	* 3. In the event of a tie the attacking piece wins
+	* 
+	*/
+
+}
